c38bfd178479cb3bc039dad701cc66f1bca05d6f
Minh <4501104141@students.hcmue.edu.vn>
2024-07-30 14:12:35 +0700

    update

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -1,9 +1,9 @@
-﻿using Entities;
+﻿using Shared.DataTransferObjects;
 
 namespace Service.Contracts
 {
     public interface ICompanyService
     {
-        IEnumerable<Company> GetAllCompanies(bool trackChanges);
+        IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
     }
 }
