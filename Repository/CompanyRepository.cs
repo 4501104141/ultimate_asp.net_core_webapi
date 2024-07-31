997b8ca463e383bb7de15e89a9b1208f744ad629
Minh <4501104141@students.hcmue.edu.vn>
2024-07-31 23:45:43 +0700

    update

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -17,6 +17,6 @@ namespace Repository
         public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) => FindByCondition(x => ids.Contains(x.Id), trackChanges).ToList();
 
         public Company GetCompany(Guid companyId, bool trackChanges) => FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
-
+        public void DeleteCompany(Company company) => Delete(company);
     }
 }
