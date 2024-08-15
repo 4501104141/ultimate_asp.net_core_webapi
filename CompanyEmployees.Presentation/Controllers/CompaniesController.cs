bdadc047f95b7223b9100827d304dcf5aa8a4d79
Minh <4501104141@students.hcmue.edu.vn>
2024-08-15 21:40:22 +0700

    update

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
-﻿using CompanyEmployees.Presentation.ModelBinders;
+﻿using CompanyEmployees.Presentation.ActionFilters;
+using CompanyEmployees.Presentation.ModelBinders;
 using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 using Shared.DataTransferObjects;
@@ -28,6 +29,7 @@ namespace CompanyEmployees.Presentation.Controllers
         }
 
         [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
         public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDto company)
         {
             if (company is null)
@@ -58,6 +60,7 @@ namespace CompanyEmployees.Presentation.Controllers
         }
 
         [HttpPut("{id:guid}")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
         public async Task<IActionResult> UpdateCompany(Guid id, [FromBody] CompanyForUpdateDto company)
         {
             if (company is null)
--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -2,23 +2,22 @@
 using Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Repository
+namespace Repository;
+
+internal sealed class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
 {
-    public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+    public EmployeeRepository(RepositoryContext repositoryContext) : base(repositoryContext)
     {
-        public EmployeeRepository(RepositoryContext repositoryContext) : base(repositoryContext)
-        {
-        }
-
-        public void CreateEmployeeForCompany(Guid companyId, Employee employee)
-        {
-            employee.CompanyId = companyId;
-            Create(employee);
-        }
-
-        public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) => await FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges).SingleOrDefaultAsync();
-
-        public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges) => await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).OrderBy(e => e.Name).ToListAsync();
-        public void DeleteEmployee(Employee employee) => Delete(employee);
     }
+
+    public void CreateEmployeeForCompany(Guid companyId, Employee employee)
+    {
+        employee.CompanyId = companyId;
+        Create(employee);
+    }
+
+    public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) => await FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges).SingleOrDefaultAsync();
+
+    public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges) => await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).OrderBy(e => e.Name).ToListAsync();
+    public void DeleteEmployee(Employee employee) => Delete(employee);
 }
